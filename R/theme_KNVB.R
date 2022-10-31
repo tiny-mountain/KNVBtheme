ec52baee0ebf089ae4cfc419b9721217acf045b8
Tom Bergkamp <61659679+tiny-mountain@users.noreply.github.com>
2022-10-31 16:36:47 +0100

    added namespace to functions

--- R/theme_KNVB.R
+++ R/theme_KNVB.R
@@ -9,8 +9,8 @@ theme_knvb <- function(base_size = 12,
       plot.margin = grid::unit(rep(0.5, 4), "cm"),
 
       # set size and color of grid lines
-      panel.grid.major = element_line(color = "gray60", size = 0.25),
-      panel.grid.minor = element_line(color = "gray65", size = 0.1),
+      panel.grid.major = ggplot2::element_line(color = "gray60", size = 0.25),
+      panel.grid.minor = ggplot2::element_line(color = "gray65", size = 0.1),
 
       # adjust panel background  and remove border
       panel.background =  ggplot2::element_rect(fill = "transparent", color = NA),
@@ -22,7 +22,7 @@ theme_knvb <- function(base_size = 12,
       axis.ticks = ggplot2::element_blank(),
 
       # modify the bottom margins of the title and subtitle
-      plot.title = ggplot2::element_text(size = rel(2), colour = "#3C3C3C",
+      plot.title = ggplot2::element_text(size = ggplot2::rel(2), colour = "#3C3C3C",
                                          hjust = 0,
                                          margin = ggplot2::margin(b = 10)),
       plot.subtitle = ggplot2::element_text(size = ggplot2::rel(1.5), colour = "#A0A0A3",
@@ -32,18 +32,18 @@ theme_knvb <- function(base_size = 12,
       # adjust legend
       legend.position = "top",
       legend.direction = "horizontal",
-      legend.background = element_blank(),
+      legend.background = ggplot2::element_blank(),
 
-      legend.key = element_rect(fill = "transparent",
+      legend.key = ggplot2::element_rect(fill = "transparent",
                                 color = NA),
 
       legend.title.align = 1,
       legend.box.just = "left",
-      legend.box.spacing =  unit(0.25, "cm"),
-      legend.spacing.x =  unit(0.25, "cm"),
+      legend.box.spacing =  grid::unit(0.25, "cm"),
+      legend.spacing.x =  grid::unit(0.25, "cm"),
 
-      legend.text = element_text(size = rel(.9), color = "#3C3C3C"),
-      legend.title = element_text(size = rel(1), color = "#3C3C3C"),
+      legend.text = ggplot2::element_text(size = ggplot2::rel(.9), color = "#3C3C3C"),
+      legend.title = ggplot2::element_text(size = ggplot2::rel(1), color = "#3C3C3C"),
       # # #legend.text.align = 1,
       # legend.text = element_text(lineheight = 10,
       #                             size = rel(.8),
@@ -58,14 +58,14 @@ theme_knvb <- function(base_size = 12,
       strip.placement = "outside",
 
       # Adjust text size and axis title position
-      axis.title.x = ggplot2::element_text(size = rel(1.2),
+      axis.title.x = ggplot2::element_text(size = ggplot2::rel(1.2),
                                            colour = "#3C3C3C",
-                                           margin = margin(t = 5)),
-      axis.title.y = ggplot2::element_text(size = rel(1.2),
+                                           margin = ggplot2::margin(t = 5)),
+      axis.title.y = ggplot2::element_text(size = ggplot2::rel(1.2),
                                            colour = "#3C3C3C",
-                                           margin = margin(r = 10),
+                                           margin = ggplot2::margin(r = 10),
                                            angle = 90),
-      axis.text = ggplot2::element_text(size = rel(.9), colour = "#3C3C3C")
+      axis.text = ggplot2::element_text(size = ggplot2::rel(.9), colour = "#3C3C3C")
     )
 
 }
