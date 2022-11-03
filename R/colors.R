96d76489773444e478a5333745eb9a6bd2fd8ca2
Tom Bergkamp <61659679+tiny-mountain@users.noreply.github.com>
2022-11-03 09:03:35 +0100

    add functions for color palette and descriptions

---
+++ R/colors.R
@@ -0,0 +1,35 @@
+#' KNVB color palette
+#'
+#' The standard nine-color KNVB palette for line plots comprises different
+#' shades of orange, blue, cyan, and gray
+#'
+#' @export
+knvb_pal <- function() {
+  values <- KNVBtheme::knvb_cols$value
+  max_n <- 9
+  f <- scales::manual_pal(values)
+  attr(f, "max_n") <- max_n
+  f
+}
+
+
+#' KNVB color scales
+#'
+#' Color scales using the KNVB colors
+#'
+#' @inheritParams ggplot2::scale_colour_hue
+#' @rdname scale_knvb
+#' @export
+scale_colour_knvb <- function(...) {
+  ggplot2::discrete_scale("colour", "economist", knvb_pal(), ...)
+}
+
+#' @rdname scale_knvb
+#' @export
+scale_color_knvb <- scale_colour_knvb
+
+#' @rdname scale_knvb
+#' @export
+scale_fill_knvb <- function(...) {
+  ggplot2::discrete_scale("fill", "economist", knvb_pal(), ...)
+}
