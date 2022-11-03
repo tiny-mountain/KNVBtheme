3027a688880451000602463e8e3b303c17674ab6
Tom Bergkamp <61659679+tiny-mountain@users.noreply.github.com>
2022-11-03 09:24:11 +0100

    add color documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Dataframe with KNVB colors used in palette functions
+#'
+#' @format ## `knvb_cols`
+#' #' A data frame with 18 rows and 2 columns:
+#' \describe{
+#'   \item{name}{Color name}
+#'   \item{value}{RGB hex value}
+#'   }
+#'
+"knvb_cols"
+
+#' List with KNVB colors, used to set colors manually
+#'
+"knvb_cols_ls"
