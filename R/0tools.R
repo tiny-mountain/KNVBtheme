3502fb73b5b804a34b7b80c2e318179049495f10
Tom Bergkamp <61659679+tiny-mountain@users.noreply.github.com>
2022-11-08 14:29:03 +0100

    change path to fonts in /inst/font

--- R/0tools.R
+++ R/0tools.R
@@ -4,12 +4,14 @@
 
   showtext::showtext_auto()
 
-  # set names for fonts to be used in graphs, combine with paths
-  font <- list.files("inst/font") %>%
-    gsub("_von_Wesseling|.otf", "", .) %>%
-    data.frame(name = ., path = paste0("inst/font/", list.files("inst/font")))
+  font <- system.file("font", package = "KNVBtheme") %>% list.files(full.names = T)
+  font_name <- system.file("font", package = "KNVBtheme") %>% list.files(full.names = F)
+  font_name <- gsub("_von_Wesseling|.otf", "", font_name)
+  font_df <- data.frame(name = font_name, path = font)
 
   # load fonts for showtext
-  mapply(sysfonts::font_add, font$name, font$path) %>%
+  mapply(sysfonts::font_add, font_df$name, font_df$path) %>%
     invisible()
   }
+
+
